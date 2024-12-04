838bd699ab1ff454a47c1e4204e6439c41af862c
Lais <laisbatistapereira@gmail.com>
2024-12-03 22:51:49 -0300

    ✨ feat: add product types and implement products list component with data fetching.

---
+++ capputeeno/src/components/products-list.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useProducts } from "@/hooks/useProducts";
+
+interface ProductsListProps {}
+
+export function ProductsList(props: ProductsListProps) {
+  const { data } = useProducts();
+  console.log(data);
+  return <></>;
+}
